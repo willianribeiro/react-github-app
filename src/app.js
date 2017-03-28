4046e17fe12efb083c7cf3c3e9a25956af03ebac
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-03-28 19:46:49 -0300

    Add first base layout

--- src/app.js
+++ src/app.js
@@ -5,35 +5,52 @@ import React, { Component } from 'react'
 import './css/style.css'
 
 class App extends Component {
-  constructor () {
-    super()
-    this.state = {
-      title: '...',
-      Component: 'div'
-    }
-  }
-
-  getTitle () {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve('My app with async / await!')
-      }, 2000)
-    })
-  }
-
-  async componentDidMount () {
-    const title = await import('components/title')
-
-    this.setState({
-      title: await this.getTitle(),
-      Component: title.default
-    })
-  }
-
   render () {
     return (
-      <div>
-        <this.state.Component>{this.state.title}</this.state.Component>
+      <div className='app'>
+        <div className="search">
+          <input 
+            type="search"
+            placeholder="Digite o nome do usuário no Github"
+          />
+        </div>
+
+        <div className='user-info'>
+          <img src="https://avatars2.githubusercontent.com/u/3102551?v=3" alt="Foto de perfil"/>
+          <h1>
+            <a href='https://github.com/willianribeiro'>Willian Ribeiro</a>
+          </h1>
+
+          <ul className='repos-info'>
+            <li>Repositórios: 19</li>
+            <li>Seguidores: 20</li>
+            <li>Seguindo: 36</li>
+          </ul>
+
+          <div className="actions">
+            <button>Ver repositórios</button>
+            <button>Ver favoritos</button>
+          </div>
+
+          <div className="repos">
+            <h2>Repositórios:</h2>
+            <ul>
+              <li>
+                <a href="">Nome do repositório</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="starred">
+            <h2>Favoritos:</h2>
+            <ul>
+              <li>
+                <a href="">Nome do repositório</a>
+              </li>
+            </ul>
+          </div>
+
+        </div>
       </div>
     )
   }
