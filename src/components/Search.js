58fab237950beb91f831a2a327cea78c9117044a
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-03-29 23:31:43 -0300

    Add function to get user from api

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,24 @@
 'use strict'
 
 import React from 'react'
+import ajax from '@fdaciuk/ajax'
 
 const Search = ({ children }) => (
   <div className='search'>
     <input
       type='search'
       placeholder='Digite o nome do usuário no Github'
+      onKeyUp={(e) => {
+        const ENTER = 13
+        const key = e.which || e.keyCode
+        const value = e.target.value
+
+        if (key === ENTER) {
+          ajax()
+            .get(`https://api.github.com/users/${value}`)
+            .then((result) => console.log(result))
+        }
+      }}
     />
   </div>
 )
